ad3e7c6c413541f08b0ac52e04a30315f68edbbc
Mallepellyvaishnavi <mallepally.vaishnavi@gmail.com>
2024-06-24 17:14:17 +0530

    Update StringReverse.java

--- StringReverse.java
+++ StringReverse.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class StringReverse {
 	public static void main(String args[])
 	{
-		Scanner scanner=new Scanner(System.in);
+		Scanner scanner=new Scanner(System.in);//using the   input scanner
 		System.out.println("enter a string:");
 		String str=scanner.nextLine();
 
-		char ch[] = str.toCharArray();
-      for(int i=0;i<str.length();i++)
+		char ch[] = str.toCharArray(); // strings into arrays
+      for(int i=0;i<str.length();i++) //logic intiliazation for loop
       {
     	  System.out.print(ch[i]);
       }
@@ -19,7 +19,7 @@ public class StringReverse {
       }
     	  for(int j=str.length()-1;j>=0;j--)
     	  {
-    		  System.out.print(ch[j]+" ");
+    		  System.out.print(ch[j]+" "); //printing the reverse string 
     	  }
     	  System.out.println();
       }
